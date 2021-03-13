3790e54114228dfc06bf41fab5fdb75115ecf5c0
Brendon Ky <brendonky18@users.noreply.github.com>
2021-03-13 17:00:53 -0500

    Updated EISA constants

--- src/eisa.py
+++ src/eisa.py
@@ -1,29 +1,24 @@
 from constant import const
+class EISA_properties(object):
+    @const
+    def WORD_SIZE(self): return 32 # the number of bits in each word
+    @const
+    def ADDRESS_SIZE(self): return 8 # the number bits in the address space
+    @const
+    def OFFSET_SIZE(self): return 2 # the number of bits for the offset in a cache line
+    @const
+    def CACHE_SIZE(self): return 4 # the number of addressable bits used by the cache
+    @const
+    def RAM_SIZE(self): return 8 # the number of addressable bits used by RAM
 
-@const
-def WORD_SIZE(): return 32 # the number of bits in each word
+    # wrappers
+    @const
+    def ADDRESS_SPACE(self): return 2**ADDRESS_SIZE # the number of valid addresses
+    @const
+    def OFFSET_SPACE(self): return 2**OFFSET_SIZE # the number of words per line
+    @const
+    def CACHE_ADDR_SPACE(self): return 2**CACHE_SIZE # the number of cache lines
+    @const
+    def RAM_ADDR_SPACE(self): return 2**RAM_SIZE # the range of valid RAM addresses
 
-@const
-def ADDRESS_SIZE(): return 8 # the number bits in the address space
-
-@const
-def OFFSET_SIZE(): return 2 # the number of bits for the offset in a cache line
-
-@const
-def CACHE_SIZE(): return 4 # the number of addressable bits used by the cache
-
-@const
-def RAM_SIZE(): return 8 # the number of addressable bits used by RAM
-
-# wrappers
-@const
-def ADDRESS_SPACE(): return 2**ADDRESS_SIZE # the number of valid addresses
-
-@const
-def OFFSET_SPACE(): return 2**OFFSET_SIZE # the number of words per line
-
-@const
-def CACHE_ADDR_SPACE(): return 2**CACHE_SIZE # the number of cache lines
-
-@const
-def RAM_ADDR_SPACE(): return 2**RAM_SIZE # the range of valid RAM addresses
+EISA = EISA_properties()
