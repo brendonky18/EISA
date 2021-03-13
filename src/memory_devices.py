c68b58cc5ad4d97fc37a7a689fb966d124353b98
Brendon Ky <brendonky18@users.noreply.github.com>
2021-03-12 20:58:17 -0500

    RAM can now retrieve a range of addresses

--- src/memory_devices.py
+++ src/memory_devices.py
@@ -1,20 +1,40 @@
 from memory_subsystem import MemoryDevice
-
+from typing import Union
+from functools import reduce
 class Cache(MemoryDevice):
     def __getitem__(self, address: int):
         pass
-
     
     def __setitem__(self, address: int, value: int) -> int:
         pass
 
 class RAM(MemoryDevice):
-    def __getitem__(self, address: int) -> int:
+    def __getitem__(self, address: Union[int, slice]) -> int:
+        """retreives the data from the specified address or range of addresses
+
+        Parameters
+        ----------
+        address : int or slice
+            the address or range of addresses 
+
+        Returns
+        -------
+        int
+            single combined integer representind all of the data found at the passed address(es)
+        """
+
         self.validate_address(address)
-        return self._memory[address]
 
+        if isinstance(address, int):
+            return self._memory[address]
+        elif isinstance(address, slice):
+            # combines the list of 4 words, into a single integer
+            return reduce(lambda accumulator, cur: (accumulator << 32) + cur, self._memory[address], 0) 
 
-    # Is this writing to the cache or to the memory?
+        
+
+    
+
     def __setitem__(self, address: int, value: int):
         self.validate_address(address)
         self._memory[address] = value
