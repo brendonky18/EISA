d73574bdfe2fb5cac6f657538f887cebf33670db
Brendon Ky <brendonky18@users.noreply.github.com>
2021-03-13 17:02:35 -0500

    created decorator to validate callback inputs for commandparse

--- src/commandparse.py
+++ src/commandparse.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Command:
-    arg_type: type 
+    arg_types: list[type ]
     callback: Callable[[str], Any]
 @dataclass
 class UserInput:
@@ -24,7 +24,7 @@ class CommandParser:
         self.valid_commands = {}
 
     # add a command which the terminal will recognize
-    def add_command(self, cmd: str, arg_type: type, callback: Callable[[str], Any]) -> bool:
+    def add_command(self, cmd: str, arg_types: list[type], callback: Callable[..., Any]) -> bool:
         """Adds a command to the terminal
 
         Parameters
@@ -43,13 +43,12 @@ class CommandParser:
         bool
             True if the command was added, False if the command already exists
         """
-        
 
         # adds the command to the dictionary of valid commands, unless it already exists
         if cmd in self.valid_commands:
             return False
         else:
-            self.valid_commands[cmd] = Command(arg_type , callback)
+            self.valid_commands[cmd] = Command(arg_types, callback)
             return True
 
     def start(self):
@@ -71,9 +70,32 @@ class CommandParser:
                 print(f'\'{cur_input.command}\' is not recognized as a command')
             else:
                 try:
+                    # invokes the designated callback, and passes the provided arguments as strings
                     self.valid_commands[cur_input.command].callback(*cur_input.args.split())
-                except TypeError: # will error on anything that isn't a literal, including strings
-                    print(f'invalid input, {cur_input.command} requires {self.valid_commands[cur_input.command].arg_type}. You entered \'{cur_input.args}\'')
+                except (TypeError, ValueError) as e: # will error on anything that isn't a literal, including strings
+                    if e is TypeError:
+                        print(f'invalid input, {cur_input.command} requires argument types ({self.valid_commands[cur_input.command].arg_types}). You entered {cur_input.args}')
+                    else:
+                        print(f'invalid input, {cur_input.command} requires {len(self.valid_commands[cur_input.command].arg_types)} arguments. You entered {len(cur_input.args)}: {cur_input.args}')
+
+
+def commandparse_cb(*args, arg_types:list[type]=[int], **kwargs) -> Callable[..., Any]: 
+    def commandparse_decorator(func):
+        def decorator_wrapper():
+            if not len(args) == len(arg_types):
+                raise ValueError
+            
+            casted_args = [None] * len(args)
+
+            for i in range(len(args)):
+                # cast the arguments, the 
+                casted_args[i] = arg_types[i](args[i])
+
+            return func(*casted_args)
+        return decorator_wrapper
+    return commandparse_decorator
+        
+            
 
                 
 
