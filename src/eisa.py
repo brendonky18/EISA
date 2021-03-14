bc58b1026eb319f326bc3b32d8722ef8410d9f69
mpofro7 <52636126+mpofro7@users.noreply.github.com>
2021-03-14 03:06:15 -0400

    Started implementation of getitem and setitem in cache

--- src/eisa.py
+++ src/eisa.py
@@ -1,24 +1,27 @@
-from src import constant as c
+from constant import const
+class EISA_properties(object):
+    @const
+    def WORD_SIZE(self): return 32 # the number of bits in each word
+    @const
+    def ADDRESS_SIZE(self): return 8 # the number bits in the address space
+    @const
+    def OFFSET_SIZE(self): return 2 # the number of bits for the offset in a cache line
+    @const
+    def CACHE_SIZE(self): return 4 # the number of addressable bits used by the cache
+    @const
+    def RAM_SIZE(self): return 8 # the number of addressable bits used by RAM
 
-@c.const
-def WORD_SIZE(self): return 32 # the number of bits in each word
-@c.const
-def ADDRESS_SIZE(self): return 8 # the number bits in the address space
-@c.const
-def OFFSET_SIZE(self): return 2 # the number of bits for the offset in a cache line
-@c.const
-def CACHE_SIZE(self): return 4 # the number of addressable bits used by the cache
-@c.const
-def RAM_SIZE(self): return 8 # the number of addressable bits used by RAM
+    # wrappers
+    @const
+    def ADDRESS_SPACE(self): return 2**self.ADDRESS_SIZE # the number of valid addresses
+    @const
+    def OFFSET_SPACE(self): return 2**self.OFFSET_SIZE # the number of words per line
+    @const
+    def CACHE_ADDR_SPACE(self): return 2**self.CACHE_SIZE # the number of cache lines
+    @const
+    def RAM_ADDR_SPACE(self): return 2**self.RAM_SIZE # the range of valid RAM addresses
+    @const
+    def WORD_INT_LIM(self): return 2 **self.WORD_SIZE  # the range of valid RAM addresses
 
-# wrappers
-@c.const
-def ADDRESS_SPACE(self): return 2 **self.ADDRESS_SIZE  # the number of valid addresses
-@c.const
-def OFFSET_SPACE(self): return 2**self.OFFSET_SIZE # the number of words per line
-@c.const
-def CACHE_ADDR_SPACE(self): return 2**self.CACHE_SIZE # the number of cache lines
-@c.const
-def RAM_ADDR_SPACE(self): return 2**self.RAM_SIZE # the range of valid RAM addresses
 
-#EISA = EISA_properties()
+EISA = EISA_properties()
