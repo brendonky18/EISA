244dd6f5ac8f25af542f1b4fe36e47710e6ff273
Brendon Ky <brendonky18@users.noreply.github.com>
2021-03-14 22:07:31 -0400

    updated CacheWay
    implemented to string
    moved bitfield property constructor to be a function in CacheWay

--- src/memory_devices.py
+++ src/memory_devices.py
@@ -32,7 +32,7 @@ class CacheWay:
     _index_start: int
     _data_start: int = 0
 
-    def __init__(self, tag_bits: int, index_bits: int, offset_bits: int):
+    def __init__(self, index_bits: int, offset_bits: int):
         """constructor for a cache way
 
         Parameters
@@ -45,7 +45,9 @@ class CacheWay:
             the number of bits in the offset field
         """
 
-        self._tag_bits = tag_bits
+        self._tag_bits = EISA.ADDRESS_SIZE - index_bits - offset_bits
+        if self._tag_bits < 0: # ensure that the tag field is at least 0
+            raise TypeError('index bits and offset bits are larger than the address size')
         self._index_bits = index_bits
         self._offset_bits = offset_bits
         
@@ -59,25 +61,30 @@ class CacheWay:
         self._dirty_start = self._tag_start + self._tag_bits
         self._valid_start = self._dirty_start + 1
 
-        # creates the accessor functions
-        self.valid = bitfield_property_constructor(self._valid_start, 1)
-        self.dirty = bitfield_property_constructor(self._dirty_start, 1)
-        self.tag = bitfield_property_constructor(self._tag_start, self._tag_bits)
-        self.index = protected_bitfield_property_constructor(self._index_start, self._index_bits)
-        self.data = protected_bitfield_property_constructor(0, self._data_bits)
-
-        # initializes values
-        self.valid(False)
-        self.dirty(False)
-        self.tag(0)
-        self.index(0)
-        self.data(0)
+        # creates the accessor functions, and initialize values
+        self.valid = self.bitfield_property_constructor(self._valid_start, 1, False)
+        self.dirty = self.bitfield_property_constructor(self._dirty_start, 1, False)
+        self.tag = self.bitfield_property_constructor(self._tag_start, self._tag_bits, 0)
+        self.index = self.protected_bitfield_property_constructor(self._index_start, self._index_bits, 0)
+        self.data = self.protected_bitfield_property_constructor(0, self._data_bits, 255)
 
     def __str__(self) -> str:
         """to string method
         """
         # Print starting line
-        s = tabulate([['Valid', self.valid()], ['Dirty', self.dirty()], ['Tag', self.tag()], ['Index', self.index()], ['Data', self.data()]])
+        s = tabulate(
+            [
+                ['Valid',   f'{self.valid():#0{2}x}'], 
+                ['Dirty',   f'{self.dirty():#0{2}x}'], 
+                ['Tag',     f'{self.tag():#0{2 + self._tag_bits//4}x}'], 
+                ['Index',   f'{self.index():#0{2 + self._index_bits//4}x}'], 
+                ['Data',    f'{self.data():#0{2 + self._data_bits//4}x}']
+            ],
+            headers=['Field', 'Value'],
+            tablefmt='pretty',
+            stralign='left',
+            numalign='right'
+            )
 
         return s
     
@@ -115,10 +122,9 @@ class CacheWay:
         """
         pass
     
-    def bitfield_property_constructor(self, start, size) -> Callable[[Any], Any]: # this becomes the decorator
-        print('constructor')
-        
-        def bitfield_property(self, value: Optional[int]=None) -> Any: 
+    def bitfield_property_constructor(self, start: int, size: int, initial_val: int) -> Callable[[Any], Any]: # this becomes the decorator
+        # create accessor function
+        def bitfield_property(value: Optional[int]=None) -> Any: 
             # get
             if value is None:
                 print('get')
@@ -126,19 +132,32 @@ class CacheWay:
             # set
             else:
                 print("set")
-                self._entry &= ~(((2**size) - 1) << self._valid_start) # clears the original value
+                self._entry &= ~(((2**size) - 1) << start) # clears the original value
                 self._entry |= value << start # assigns the value
+
+        # initialize value
+        self._entry &= ~(((2**size) - 1) << start) # clears the original value
+        self._entry |= initial_val << start # assigns the value
 
         return bitfield_property
 
-    def protected_bitfield_property_constructor(self, start: int, size: int):
-        def bit_field_set(self, value):
-            raise TypeError('cannot assign values to a protected bitfield')
+    def protected_bitfield_property_constructor(self, start: int, size: int, initial_val: int):
+        # create protected accessor function
+        def protected_bitfield_property(value: Optional[int]=None) -> Any: 
+            if value is None:
+                return int(self._entry >> start)
+            else:
+                raise TypeError('cannot assign values to a protected bitfield')
 
-        def bit_field_get(self):
-            return bool(self._entry >> self._valid_start)
+        # initialize value
+        self._entry &= ~(((2**size) - 1) << start) # clears the original value
+        self._entry |= initial_val << start # assigns the value
 
-        return property(bit_field_get, bit_field_set)
+        print(f'init val: {initial_val}')
+        print(f'value initialized: {protected_bitfield_property()}')
+
+        return protected_bitfield_property
+
 # TODO: extra credit, use to implement associative caches instead of direct-mapped
 class CacheBlock():
     pass
@@ -174,7 +193,7 @@ class Cache(MemoryDevice):
         """
         super().__init__(addr_size, next_device, read_speed, write_speed)
         self._offset_bits = offset_bits
-        self._cache = [CacheWay(EISA.ADDRESS_SIZE - self._addr_size, self._addr_size, offset_bits)] * EISA.CACHE_ADDR_SPACE
+        self._cache = [CacheWay(self._addr_size, offset_bits)] * EISA.CACHE_ADDR_SPACE
            
     #TODO implement data structure for cache
     def __getitem__(self, address: int):
@@ -182,8 +201,6 @@ class Cache(MemoryDevice):
     
     def __setitem__(self, address: int, value: int) -> int:
         pass
-
-
 
 class RAM(MemoryDevice):
     def __getitem__(self, address: Union[int, slice]) -> int:
@@ -213,3 +230,7 @@ class RAM(MemoryDevice):
         self._memory[address] = value
 
 
+
+#debugging
+if __name__ == '__main__':
+    c = CacheWay(4, 2)
