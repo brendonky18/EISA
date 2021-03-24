d7244e314f7ef75fabf3095c465c30c835e83fd4
mpofro7 <52636126+mpofro7@users.noreply.github.com>
2021-03-24 00:39:43 -0400

    pipeline prototyped

--- src/pipeline.py
+++ src/pipeline.py
@@ -6,18 +6,18 @@ class Instruction:
     # Operation
     _opcode: int
 
-    # Register addresses
+    # Register addresses (raw values provided to instruction)
     _regA: int
     _regB: int
     _regC: int
 
-    # Operands
+    # Operands (values loaded assuming register addresses in regA,regB,regC)
     _opA: int
     _opB: int
     _opC: int
 
-    # Result if the instruction is a computation. None for loads and stores
-    _computed: int
+    # Result for addition instructions, or loaded value if a load instruction
+    _computed: None
 
     # Determine whether opB is immediate
     _immediate: int
@@ -27,6 +27,7 @@ class Instruction:
 
     def __init__(self, encoded: int):
         self.encoded = encoded
+        self.computed = None
 
     def decode(self):
         self.opcode = self.encoded >> 28
@@ -37,6 +38,8 @@ class Instruction:
 
 class PipeLine:
     # TODO - Use dict later to get funcs associated with opcodes
+    # TODO - Implement branching and branch squashing
+    # TODO - implement no-ops and stalls
 
     _memory: MemorySubsystem
     _pc: None
@@ -48,46 +51,74 @@ class PipeLine:
     _em_reg: Queue[Instruction]
     _mw_reg: Queue[Instruction]
 
+    _cycles: int
+
     def __init__(self, pc: int, registers: list[int], cache_size: int, ram_size: int):
         self.registers = registers
         self.pc = pc
         self.memory = MemorySubsystem(cache_size, ram_size)
         self.pipeline = [None for i in range(5)]
+        self._fd_reg = Queue()
+        self._de_reg = Queue()
+        self._em_reg = Queue()
+        self._mw_reg = Queue()
+        self.cycles = 0
 
     def stage_fetch(self):
         # Load instruction in MEMORY at the address the PC is pointing to
-        incoming_instruction = self.memory[self.pc]
+        instruction = self.memory[self.pc]
+        self.pipeline[0] = instruction
 
         # increment PC by 1 word
         self.pc += 1
 
         # push instruction into queue
-        self.pipeline[0] = incoming_instruction
-        self.fd_reg.put(incoming_instruction)
+        self.pipeline[0] = instruction
+        self.fd_reg.put(instruction)
 
     def stage_decode(self):
 
         # get fetched instruction
         instruction = self.fd_reg.get()
+        self.pipeline[1] = instruction
 
         # Decode the instruction
         instruction.decode()
 
         # Access regfile to read the registers
         opA = self.registers[instruction.regA]
-        opB = self.registers[instruction.regB]
+        opC = self.registers[instruction.regC]
+
+        # Determine if second operand is immediate or not
+        if instruction.immediate:
+            opB = instruction.regB
+        else:
+            opB = self.registers[instruction.regB]
 
         # Put outputs of GP regs into two temp regs (for use later)
         #   Storing them inside the instruction itself
         instruction.opA = opA
         instruction.opB = opB
+        instruction.opC = opC
 
         # TODO? - Sign extend the lower 16 bits of the instr. reg and store
         #   into temp Imm reg
 
+        # Push edited instruction into the adjacent queue
+        self.de_reg.put(instruction)
+
     def stage_execute(self):
+
+        # get decoded instruction
+        instruction = self.de_reg.get()
+        self.pipeline[2] = instruction
+
         # Execute depending on instruction
         # https://en.wikipedia.org/wiki/Classic_RISC_pipeline
+
+        # Addition
+        if instruction.opcode == 0b000011:
+            instruction.computed = instruction.opA + instruction.opB
 
         # 1) Memory reference. Obtain values located at referred addr
         #   (2-cycle latency)
@@ -95,28 +126,52 @@ class PipeLine:
         # 2) Reg-Reg. Add, subtract, compare, and logical operations.
         #   (1-cycle latency). Dictated by opcode
 
-        pass
+        # Push edited instruction into the adjacent queue
+        self.em_reg.put(instruction)
 
     def stage_memory(self):
-        # Update PC
+
+        # get executed instruction
+        instruction = self.em_reg.get()
+        self.pipeline[3] = instruction
 
         # If instruction is a load, data return from memory and are placed in the LMD reg
+        if instruction.opcode == 0b010001:
+
+            # Load value obtained from address Rn + Rt in memory into instruction
+            instruction.computed = self.memory[instruction.regB + instruction.regC]
 
         # If the instruction is a store, then the data from reg B is written into memory
+        elif instruction.opcode == 0b010000:
 
-        # Address used is the one computed during the prior cycle and stored in the ALU output reg
+            # Store value in register Rm into MEMORY address Rn + Rt
+            # Address used is the one computed during the prior cycle and stored in the ALU output reg
+            self.memory[instruction.opB + instruction.opC] = instruction.opA
 
-        pass
+        # Push edited instruction into the adjacent queue
+        self.mw_reg.put(instruction)
 
     def stage_writeback(self):
+
+        # get memorized instruction
+        instruction = self.mw_reg.get()
+        self.pipeline[4] = instruction
+
         # Write the result into the reg file, whether it comes from the memory system (LMD reg) or
         #   the ALU (ALUOutput).
-
-        pass
+        if instruction.computed is not None:
+            if instruction.opcode == 0b000011:
+                self.registers[instruction.regC] = instruction.computed
+            elif instruction.opcode == 0b010001:
+                self.registers[instruction.regA] = instruction.computed
 
     def cycle_pipeline(self):
 
         # TODO - Process one step in the pipeline
-
-        pass
+        self.stage_fetch()
+        self.stage_decode()
+        self.stage_execute()
+        self.stage_memory()
+        self.stage_writeback()
+        self.cycles += 1
 
